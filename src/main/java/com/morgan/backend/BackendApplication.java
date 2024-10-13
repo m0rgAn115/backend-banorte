c846824cbd59705065a8bdcbfd98bc12e9d639f2
Angel Mondragon <damianmor3005@hotmail.com>
2024-10-13 06:04:29 -0600

    nuevo endpoint

--- src/main/java/com/morgan/backend/BackendApplication.java
+++ src/main/java/com/morgan/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.morgan.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class BackendApplication {
 
+	@CrossOrigin(origins = "*")
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
